85fddeae235ab97cf97a4e432bf9c87c3d46c0ca
Linux Main <kgmcknc@gmail.com>
2017-10-25 22:38:18 -0400

    Initial Commit for Media_Server bringing in Main and Pi side folders from development

---
+++ Main/deploy.sh
@@ -0,0 +1,8 @@
+# This file deploys the Linux side Main folder to the source directory
+# Calling this file forces the git depot into the deploy folder
+
+echo "Deploying Linux Main"
+
+echo "Copying All Files to /usr/share/myfolder/"
+
+echo "Updated Linux Main Files"

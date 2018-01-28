08549e1211f98a4acf924f3c494708b3f5e5b06f
Kyle <kgmcknc@gmail.com>
2018-01-27 23:07:55 -0500

    Updated Deploy Shell Script for new paths and added auto common code. Excluded .kmf in ignore

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,62 @@
 # This file deploys the Media_Server application code into your system
 # Calling this file forces the git depot into the deploy folder
+# This file will auto-run makefiles for all deployed c programs
+# Specify Server or Client as an input argument when calling this script
 
-if [ "$#" -eq 0 ]; then
+if [ "$#" -eq 0 ] ; then
 	echo "Error! Needs command line argument"
-	echo "Specify Main or Pi for which side to deploy"
+	echo "Specify Server or Client for which side to deploy"
+	echo "Common code automatically deployed unless -nc argument is appended"
 	exit
 else
-	if [ "$1" = "Main" ] ; then
-		echo "Arg was Main!"
-		deploy_main=1;
-		deploy_pi=0;
-		deploy_common=0;
-	else
-		if [ "$1" = "Pi" ] ; then
-			echo "Arg was Pi!"
-			deploy_main=0;
-			deploy_pi=1;
-			deploy_common=0;
-		else
-			if [ "$1" = "Common" ] ; then
-				echo "Arg was Common!"
-				deploy_main=0;
-				deploy_pi=0;
-				deploy_common=1;
+	if [ "$1" = "Server" ] || [ "$1" = "server" ] ; then
+		echo "Arg was Server!"
+		deploy_server=1;
+		deploy_client=0;
+		if [ "$#" -eq 2 ] ; then
+			if [ "$2" = "-nc" ] ; then
+				deploy_common=0;
+				echo "Not Copying Common Code!"
 			else
-				echo "Error! Not valid command line argument"
-				echo "Specify Main or Pi for which side to deploy"
+				deploy_common=1;
+			fi
+		else
+			deploy_common=1;
+		fi
+	else
+		if [ "$1" = "Client" ] || [ "$1" = "client" ] ; then
+			echo "Arg was Client!"
+			deploy_server=0;
+			deploy_client=1;
+			if [ "$#" -eq 2 ] ; then
+				if [ "$2" = "-nc" ] ; then
+					deploy_common=0;
+					echo "Not Copying Common Code!"
+				else
+					deploy_common=1;
+				fi
+			else
+				deploy_common=1;
+			fi
+		else
+			if [ "$1" = "Common" ] || [ "$1" = "common" ] ; then
+				echo "Arg was Common!"
+				deploy_server=0;
+				deploy_client=0;
+				if [ "$#" -eq 2 ] ; then
+					if [ "$2" = "-nc" ] ; then
+						deploy_common=0;
+						echo "Not Copying Common Code!"
+					else
+						deploy_common=1;
+					fi
+				else
+					deploy_common=1;
+				fi
+			else
+					echo "Error! Needs command line argument"
+					echo "Specify Server or Client for which side to deploy"
+					echo "Common code automatically deployed unless -nc argument is appended"
 				exit
 			fi
 		fi
@@ -36,10 +67,10 @@ sys_deploy_dir="/usr/share/media_server/"
 www_deploy_dir="/var/www/html/media_server/"
 rx_fifo="/var/www/html/media_server/control/web_control/rxwebfifo"
 
-if [ "$deploy_main" -eq 1 ] ; then
+if [ "$deploy_server" -eq 1 ] ; then
 	echo "Deploying Server Side System Code to $sys_deploy_dir"
 fi
-if [ "$deploy_pi" -eq 1 ] ; then
+if [ "$deploy_client" -eq 1 ] ; then
 	echo "Deploying Client Side System Code to $sys_deploy_dir"
 fi
 if [ "$deploy_common" -eq 1 ] ; then
@@ -61,13 +92,13 @@ fi
 
 echo "Copying All Files to $sys_deploy_dir"
 
-if [ "$deploy_main" -eq 1 ] ; then
-	for file_name in ./Main/sys/*; do
+if [ "$deploy_server" -eq 1 ] ; then
+	for file_name in ./Server/sys/*; do
 		cp -r "$file_name" "$sys_deploy_dir"
 	done
 fi
-if [ "$deploy_pi" -eq 1 ] ; then
-	for file_name in ./Pi/sys/*; do
+if [ "$deploy_client" -eq 1 ] ; then
+	for file_name in ./Client/sys/*; do
 		cp -r "$file_name" "$sys_deploy_dir"
 	done
 fi
@@ -77,11 +108,11 @@ if [ "$deploy_common" -eq 1 ] ; then
 	done
 fi
 
-if [ "$deploy_main" -eq 1 ] ; then
+if [ "$deploy_server" -eq 1 ] ; then
 	echo "Deployed Server Side System Files"
 	echo "Deploying Server Side Web Code to $www_deploy_dir"
 fi
-if [ "$deploy_pi" -eq 1 ] ; then
+if [ "$deploy_client" -eq 1 ] ; then
 	echo "Deployed Client Side System Files"
 	echo "Deploying Client Side Web Code to $www_deploy_dir"
 fi
@@ -105,14 +136,14 @@ fi
 
 echo "Copying All Files to $www_deploy_dir"
 
-if [ "$deploy_main" -eq 1 ] ; then
-	for file_name in ./Main/www/*; do
+if [ "$deploy_server" -eq 1 ] ; then
+	for file_name in ./Server/www/*; do
 		cp -r "$file_name" "$www_deploy_dir"
 	done
 	mkfifo "$rx_fifo"
 fi
-if [ "$deploy_pi" -eq 1 ] ; then
-	for file_name in ./Pi/www/*; do
+if [ "$deploy_client" -eq 1 ] ; then
+	for file_name in ./Client/www/*; do
 		cp -r "$file_name" "$www_deploy_dir"
 	done
 	mkfifo "$rx_fifo"
@@ -124,10 +155,10 @@ if [ "$deploy_common" -eq 1 ] ; then
 fi
 
 
-if [ "$deploy_main" -eq 1 ] ; then
+if [ "$deploy_server" -eq 1 ] ; then
 	echo "Deployed Server Side WWW Files"
 fi
-if [ "$deploy_pi" -eq 1 ] ; then
+if [ "$deploy_client" -eq 1 ] ; then
 	echo "Deployed Client Side WWW Files"
 fi
 if [ "$deploy_common" -eq 1 ] ; then
